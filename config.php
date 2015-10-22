c6084ca7c7880557758f2e4f09af7d54f5c61e34
cindywelkersci <madgavmom@gmail.com>
2015-10-22 15:09:39 -0500

    Create config.php

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+return array(
+    'appsignature' => 'none',
+        'clientid' => 'n25p5oe5obmu8leb9eq58jgs',
+        'clientsecret' => 'v9El7OAseHjLhQOinbfxRvcf',
+        'defaultwsdl' => 'https://webservice.exacttarget.com/etframework.wsdl'
+);
+?>
